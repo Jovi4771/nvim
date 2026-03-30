11d9dd5f6c81f1f846f8d41da38d2f602adcb991
qiaofei_liu <qiaofei_liu@phison.com>
2026-03-30 14:53:38 +0800

    update

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -35,8 +35,8 @@ return {
 
       vim.o.autoread = true -- Required for `opts.events.reload`
 
-      vim.keymap.set({ "n", "x" }, "<C-a>", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
-      vim.keymap.set({ "n", "x" }, "<C-x>", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
+      vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
+      vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
     end,
   }
 }
