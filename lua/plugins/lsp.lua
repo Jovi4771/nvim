f551dc471c9e513a3deafcc318d16735ea65b668
JOVI4771 <jovi.4771@gmail.com>
2026-03-31 21:12:28 +0800

    revert: remove clangd diagnostics suppress (not effective via LSP settings)

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -21,30 +21,6 @@ return {
           ".root",
           ".git"
         },
-        settings = {
-          clangd = {
-            diagnostics = {
-              suppress = {
-                "implicit",               -- 隱式函數宣告
-                "missing-header",         -- 找不到頭文件
-                "undeclared_var_use",     -- 使用未宣告的變數
-                "unknown_type",           -- 未知類型
-                "invalid_ios",            -- 無效的輸入/輸出
-                "unsupported_construct", -- 不支援的建構
-                "ambiguous-member",       -- 模糊的成員
-                "no-member",              -- 沒有成員
-                "unused",                 -- 未使用相關
-                "unused-variable",        -- 未使用的變數
-                "unused-function",        -- 未使用的函數
-                "unused-parameter",       -- 未使用的參數
-                "unused-include",         -- 未使用的 include
-                "err_unknown_type",       -- Clang 內部：錯誤未知類型
-                "err_undeclared",         -- Clang 內部：錯誤未宣告
-                "warn_implicit_decl",     -- Clang 內部：警告隱式宣告
-              },
-            },
-          },
-        },
       })
 
       vim.lsp.enable("lua_ls")
