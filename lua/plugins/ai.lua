a4e1957b8fb97beb7c10104fdf0fbd67774d3145
qiaofei_liu <qiaofei_liu@phison.com>
2026-05-06 16:06:15 +0800

    update

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -28,15 +28,37 @@ return {
       },
     },
     config = function()
+      local opencode_cmd = 'opencode --port'
+      ---@type snacks.terminal.Opts
+      local snacks_terminal_opts = {
+        win = {
+          position = 'right',
+          enter = false,
+          on_win = function(win)
+            -- Set up keymaps and cleanup for an arbitrary terminal
+            require('opencode.terminal').setup(win.win)
+          end,
+        },
+      }
       ---@type opencode.Opts
       vim.g.opencode_opts = {
-        -- Your configuration, if any; goto definition on the type or field for details
+        server = {
+          start = function()
+            require('snacks.terminal').open(opencode_cmd, snacks_terminal_opts)
+          end,
+          stop = function()
+            require('snacks.terminal').get(opencode_cmd, snacks_terminal_opts):close()
+          end,
+          toggle = function()
+            require('snacks.terminal').toggle(opencode_cmd, snacks_terminal_opts)
+          end,
+        },
       }
 
       vim.o.autoread = true -- Required for `opts.events.reload`
 
       vim.keymap.set({ "n", "x" }, "<leader>oa", function() require("opencode").ask("@this: ", { submit = true }) end, { desc = "Ask opencode…" })
-      vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").select() end,                          { desc = "Execute opencode action…" })
+      vim.keymap.set({ "n", "x" }, "<leader>os", function() require("opencode").toggle() end, { desc = "Execute opencode action…" })
     end,
   }
 }
